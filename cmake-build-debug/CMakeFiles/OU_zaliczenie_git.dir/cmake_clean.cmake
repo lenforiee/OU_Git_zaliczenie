b96e5bb4c1916755b862663449c5426f68b0666b
lenforiee <lenforiee@gmail.com>
2025-01-29 20:52:19 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/OU_zaliczenie_git.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_zaliczenie_git.dir/main.cpp.o"
+  "CMakeFiles/OU_zaliczenie_git.dir/main.cpp.o.d"
+  "OU_zaliczenie_git"
+  "OU_zaliczenie_git.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OU_zaliczenie_git.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
